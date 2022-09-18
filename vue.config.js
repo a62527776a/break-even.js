f303e421f1dda2ab28417cfc03efb84659679827
chenyu <chenyu@rr.tv>
2022-09-18 20:26:55 +0800

    111

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? 'https://a62527776a.github.io/yifanshangmoniqi/dist/'
+      ? 'https://github.com/a62527776a/break-even.js/dist/'
       : '/'
   }
   
