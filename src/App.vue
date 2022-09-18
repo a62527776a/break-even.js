f7ec3de5ad14d2868dfe0b0e0124800fe86a3c02
chenyu <chenyu@rr.tv>
2022-09-18 20:20:54 +0800

    111

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+</template>
+
+<script lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: '',
+  components: {
+    HelloWorld
+  },
+  setup() {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
